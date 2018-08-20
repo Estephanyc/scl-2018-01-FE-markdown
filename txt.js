d8a1afebf902ee0b2bba21c96cc41efba37990cd
AriellaRogueN <maitilydevidasi@gmail.com>
2018-08-20 19:29:42 -0300

    eslintrc

---
+++ txt.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const Marked = require('marked');
+const fs = require('fs');
+fs.readFile('./md/file.md', 'utf8',(err, data) => {
+  if (err) throw err;
+  console.log(markdownLinkExtractor(data))
+});
+
+function markdownLinkExtractor(markdown) {
+  const links = [];
+  const renderer = new Marked.Renderer();
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+
+  renderer.link = function(href, title, text) {
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  renderer.image = function(href, title, text) {
+    // Remove image size at the end, e.g. ' =20%x50'
+    href = href.replace(/ =\d*%?x\d*%?$/, '');
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  Marked(markdown, { renderer: renderer });
+
+  return links;
+}; 
